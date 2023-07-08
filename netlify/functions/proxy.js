91ede902ddd956760627286c5b44d4c12ac515b9
Ylexis Butler <ylexislauren@gmail.com>
2023-07-08 17:36:22 -0400

    changed file directory

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-const { PEXELS_API_URL } = require("../../moodboard/constants");
+const { PEXELS_API_URL } = require("../src/constants");
 
 exports.handler = async (event, context) => {
   try {
