39b3ca11423fba37372f07df829ab859617e01ec
Ylexis Butler <ylexislauren@gmail.com>
2023-07-13 11:39:07 -0400

    added website to headers to fix CORS

--- src/components/Apiload.js
+++ src/components/Apiload.js
@@ -16,7 +16,8 @@ const Apiload = ({ id, query }) => {
     try {
       const res = await axios.get(PEXELS_API_URL(keyword, page), {
         headers: {
-          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Origin":
+            "https://ylexisb.github.io/moodboard/#/",
           "Access-Control-Allow-Headers":
             "Origin, X-Requested-With, Content-Type, Accept",
           Authorization: process.env.REACT_APP_PEXELS_API_KEY,
