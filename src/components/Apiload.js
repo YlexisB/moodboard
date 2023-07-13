f5c790905f44aa8b96e47a3bd5dbfdf1d32e6af5
Ylexis Butler <ylexislauren@gmail.com>
2023-07-13 13:46:36 -0400

    deleted  access-control-allow-credentials

--- src/components/Apiload.js
+++ src/components/Apiload.js
@@ -16,7 +16,6 @@ const Apiload = ({ id, query }) => {
     try {
       const res = await axios.get(PEXELS_API_URL(keyword, page), {
         headers: {
-          "Access-Control-Allow-Credentials": "true",
           "Access-Control-Allow-Headers":
             "Origin, Content-Type, Accept, Authorization",
           Authorization: process.env.REACT_APP_PEXELS_API_KEY,
