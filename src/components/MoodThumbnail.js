68c3386e2e4dbad598d480d8eaf0631aeeff9ffe
Ylexis Butler <ylexislauren@gmail.com>
2023-07-13 22:38:23 -0400

    updated api with giphy/js fetch package

--- src/components/MoodThumbnail.js
+++ src/components/MoodThumbnail.js
@@ -3,23 +3,38 @@ import { Link } from "react-router-dom";
 import Loader from "./Loader";
 import axios from "axios";
 import { GIPHY_API_URL } from "../constants";
+import { GiphyFetch } from "@giphy/js-fetch-api";
 
 const MoodThumbnail = ({ title, titles, setTitles, onThumbnailClick }) => {
   const [isHovering, setIsHovering] = useState(false);
   const [gifUrl, setGifUrl] = useState(null);
 
+  // useEffect(() => {
+  //   const fetchRandomColorGif = async () => {
+  //     try {
+  //       const response = await axios.get(GIPHY_API_URL, {
+  //         params: {
+  //           api_key: process.env.REACT_APP_GIPHY_API_KEY,
+  //           tag: "color",
+  //           rating: "g",
+  //         },
+  //       });
+
+  //       setGifUrl(response.data.data.images.original.url);
+  //     } catch (error) {
+  //       console.error("Error fetching random GIF:", error);
+  //     }
+  //   };
+
+  //   fetchRandomColorGif();
+  // }, []);
   useEffect(() => {
     const fetchRandomColorGif = async () => {
       try {
-        const response = await axios.get(GIPHY_API_URL, {
-          params: {
-            api_key: process.env.REACT_APP_GIPHY_API_KEY,
-            tag: "color",
-            rating: "g",
-          },
-        });
-
-        setGifUrl(response.data.data.images.original.url);
+        const giphyFetch = new GiphyFetch(GIPHY_API_URL);
+        const { data } = await giphyFetch.random({ tag: "color", rating: "g" });
+        const gifUrl = data.images.original.url;
+        setGifUrl(gifUrl);
       } catch (error) {
         console.error("Error fetching random GIF:", error);
       }
